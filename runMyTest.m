ff749ae35599a449347fe0ec0f4be42a97435338
ryo-amimoto <amimoto@cdl-net.com>
2020-09-30 17:37:24 +0900

    commit 200930_09 テスト自動化用スクリプト：runMyTest.mを修正

--- runMyTest.m
+++ runMyTest.m
@@ -1,6 +1,20 @@
-simOut = sim("Mdl_Sample");
+% RCP単体テストの自動化スクリプト         %
+% 作成者:網本　亮  作成日:2020年9月30日   %
+
+simOut = sim("Mdl_Sample");     % モデルのシミュレーション実行
+
+% モデルの入出力データを作成
 xlsHeader = {'time','InputData','OutputData'};
 xlsData = num2cell(simOut.ScopeData);
 dataSet = vertcat(xlsHeader,xlsData);
 xlswrite("OutputData.xlsx",dataSet);
-winopen("OutputData.xlsx");
+
+% モデルの期待値データを作成
+eptVal = num2cell(xlsread("ExpectValue.xlsx",1,'C:C'));
+eptHead = {'EptData'};
+eptData = vertcat(eptHead,eptVal);
+
+% モデルのテスト結果ファイルを作成
+TestData = horzcat(dataSet,eptData);
+xlswrite("TestData.xlsx",TestData);
+winopen("TestData.xlsx");
